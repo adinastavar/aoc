7059cd7bf652c994f2062ac0e86999e69a087158
Adina Gavrila <ag2436@tobii.com>
2024-11-29 08:59:54 +0200

    Git fire!

--- 2023/day3.py
+++ 2023/day3.py
@@ -1,17 +1,21 @@
 import log
+import re
+
 from dataclasses import dataclass
 from typing import List
 
 @dataclass
 class Part:
-    number : int
+    number : str
     x : int
     y : int
 
-    def __init__(self, number : int, x : int, y : int):
+    def __init__(self, number : str, x : int, y : int):
         self.number = number
         self.x      = x
         self.y      = y
+    def __str__(self):
+        return f"({self.x}, {self.y}) {self.number}"
 
 @dataclass
 class SpecialChar:
@@ -22,19 +26,38 @@ class SpecialChar:
         self.x = x
         self.y = y
 
+    def __str__(self):
+        return f"({self.x}, {self.y})"
+
 def main_first_part(filename : str):
     log.log.info("Executing part 1")
     with open(filename, "rt") as input_data:
         sum = 0
 
-        parts : List[Part] = []
-        special_chars : List[SpecialChar] = []
+        parts         : List[Part]        = []
+        # Grouping characters into lines for easier search
+        special_chars : dict[List[SpecialChar]] = {}
 
         # Extract parts and special characters
+        number_re       = re.compile(r"\d+")
+        special_char_re = re.compile(r"[^\d\.]")
 
-        # Check for parts
+        # Check for parts and special chars
+        for y, line in enumerate(input_data):
+            special_chars[y] = []
+            for m in number_re.finditer(line):
+                parts.append(Part(m.group(), m.start(), y))
+            for m in special_char_re.finditer(line):
+                special_chars[y].append(SpecialChar(int(m.start()), y))
 
+        for part in parts:
+            left_bound  = part.x - 1
+            right_bound = part.x + len(part.number)
+            top_bound = part.y - 1
+            lower_bound = part.y + 1
+            print(part)
 
+        print(special_chars)
 
         log.log.info(f"Day3 result is: {sum}")
 
--- 2023/main.py
+++ 2023/main.py
@@ -4,6 +4,7 @@ import argparse
 
 import day1
 import day2
+import day3
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 
@@ -30,6 +31,8 @@ if __name__ == "__main__":
             fn = day1.main
         case 2:
             fn = day2.main
+        case 3:
+            fn = day3.main
 
     for f in files:
         fn(f, args.part)
